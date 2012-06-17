11604bbe60b3b2ad1df6d8268b5b9d3c08dfb1a7
Matias <matias@(none).(none)>
2012-06-17 14:41:56 -0300

    Deleted unnecesary files

--- Twig/SpecialCommentsTwigExtension.php
+++ Twig/SpecialCommentsTwigExtension.php
@@ -14,20 +14,44 @@ namespace Matubaum\GenerateTemplateHelpersBundle\Twig;
 class SpecialCommentsTwigExtension extends \Twig_Extension{
     
     private $environment;
+    private $environment2;
+    private $blocks;
     
     public function getName(){
         return 'special_comments';
     }
     
-    public function initRuntime(\Twig_Environment $environment){
-        $this->environment = $environment;
-        $compiled = $this->environment->loadTemplate('MatubaumGenerateTemplateHelpersBundle::macros.html.twig');
+    
+    
+    
+    /** 
+     * Aca deberias setear la function pero asegurarte de que tellegue el compiler
+     * 
+     * @return array
+     */
+    public function getFunctions() {
+        $r = [];
         
+        
+        return $r;
     }
     
-    public function getFilters(){
-        return [
-            'var_dump' => new \Twig_Filter_Function('var_dump')
-        ];
+    function setEnvironment($environment){
+        
+        $this->environment2;
     }
+    
+    /**
+     * Aca deberias setear un bloque si es posible
+     * Pero no la funcion porque eso no anda ni por puta
+     * Todas las funciones ya se crearon a esta altura.
+     * 
+     * @param \Twig_Environment $environment
+     */
+    public function initRuntime(\Twig_Environment $environment){
+        
+        $compiled = $environment->loadTemplate('MatubaumGenerateTemplateHelpersBundle::macros.html.twig');
+        
+    }
+        
 }
